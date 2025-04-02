4c282022b5bae77af33d96f43adf2b21e2cd5b50
itsReward <69538687+itsReward@users.noreply.github.com>
2025-04-02 15:50:04 +0200

    auth

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6") // For JSON processing
 
-
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     implementation("org.json:json:20250107")
 }
 
---
+++ src/main/kotlin/com/fraudit/fraudit/config/SwaggerConfig.kt
@@ -0,0 +1,22 @@
+package com.fraudit.fraudit.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/*
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Fraudit API")
+                    .version("1.0")
+                    .description("API documentation for the Fraudit application")
+            )
+    }
+}*/
