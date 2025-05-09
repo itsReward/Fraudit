07ec23fa20744b78965abcdf3e78e6893bd2040a
itsReward <69538687+itsReward@users.noreply.github.com>
2025-05-09 15:00:38 +0200

    deployement trial

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-echo "Starting application on properly configured port"
-export SERVER_PORT=8080
-export SPRING_FLYWAY_BASELINE_ON_MIGRATE=true
-export SPRING_FLYWAY_LOCATIONS=classpath:db/migration
+# Immediately bind to port 8080
+echo "Starting port binding on 8080"
+nc -l -k -p 8080 -e 'echo -e "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nService is starting" | cat -' &
+NC_PID=$!
 
-# Force the port to be 8080
-exec java -Dserver.port=8080 -Dspring.main.web-application-type=servlet -jar /app/build/libs/fraudit.jar
+# Now start the application
+echo "Starting application..."
+java -Dserver.port=5001 -jar /app/build/libs/fraudit.jar &
+APP_PID=$!
+
+# Wait for application to exit
+wait $APP_PID
+kill $NC_PID
