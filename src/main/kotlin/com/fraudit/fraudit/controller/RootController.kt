66e4a9c5b2e99e6a94bbc7cd486f8effc2eab5f2
itsReward <69538687+itsReward@users.noreply.github.com>
2025-05-12 13:20:44 +0200

    root controller fix

--- src/main/kotlin/com/fraudit/fraudit/controller/RootController.kt
+++ src/main/kotlin/com/fraudit/fraudit/controller/RootController.kt
@@ -2,9 +2,11 @@ package com.fraudit.fraudit.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api")
 class RootController {
     @GetMapping("/")
     fun root(): ResponseEntity<String> {
