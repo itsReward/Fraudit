b65181b87ff838ad19a06773fde48c7bad7b2617
itsReward <69538687+itsReward@users.noreply.github.com>
2025-05-12 12:50:23 +0200

    deployment simplification

--- src/main/kotlin/com/fraudit/fraudit/controller/HomeController.kt
+++ src/main/kotlin/com/fraudit/fraudit/controller/HomeController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.view.RedirectView
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/app")
 class HomeController {
 
     @GetMapping
